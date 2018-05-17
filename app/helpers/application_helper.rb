02b65054fc91239ded00eb8e260bfd535a5eb672
Per Larsson <per.larsson@ub.gu.se>
2018-05-17 14:42:02 +0200

    Fix errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def handle_personalnumber raw_number
-    return nil if raw_number.blank?
-    return raw_number if raw_number.length.eql?(12)
-    return "20" + raw_number if raw_number.length.eql?(10) && /^[0]/.match(raw_number)
-    return "19" + raw_number if raw_number.length.eql?(10) && /^[^0]/.match(raw_number)
-    return nil
-  end
 end
